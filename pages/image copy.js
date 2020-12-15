242410eb42c1c684112a5ea9a8d4da1fc5eae3cb
flavioro <flavioro@gmail.com>
2020-12-15 17:05:45 -0300

    Carousel add images

--- pages/image copy.js
+++ pages/image copy.js
@@ -10,7 +10,11 @@ import WordsUpperCase from '../utils/wordsUpperCase'
 // const PREFIX_URL = 'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/';
 const PREFIX_URL = '/projetos_slides/'
 const codeHouses = projs.casas
-let images = []
+const images = []
+
+function handleImageMouseOver(e) {
+  console.log(e)
+}
 
 export default class App extends React.Component {
 
@@ -21,6 +25,7 @@ export default class App extends React.Component {
       showGalleryFullscreenButton: true,
       showPlayButton: true,
       showGalleryPlayButton: true,
+      hover: false
     };
 
     // this.images = [
@@ -55,17 +60,54 @@ export default class App extends React.Component {
           originalAlt: originalAlt,
           originalTitle: originalAlt
         }
-        this.images.push(item)
+        images.push(item)
       }
     }
+
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+
+
+  
+  toggleHover() {
+    this.setState({hover: !this.state.hover})
   }
 
   render() {
+    var linkStyle;
+    if (this.state.hover) {
+      linkStyle = {
+        color: '#ed1212',cursor: 'pointer'}
+    } else {
+      linkStyle = {color: '#000'}
+    }
+
     return (
       <section className='app'>
-        <ImageGallery
+        <ImageGallery className="hoverr"
           items={this.images}
+          onMouseOver={this.toggleHover}
+          onMouseLeave={this.toggleHover}
         />
+        <p style={linkStyle} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>Test</p>
+        <div className="container">
+          <h1>Hello Next.js</h1>
+          <p>Let's explore different ways to style Next.js apps</p>
+        </div>
+        <style jsx>{`
+          .container {
+            margin: 50px;
+          }
+          p {
+            color: blue;
+          }
+          .hoverr {
+            top: 30px!important;
+            color: blue!important;
+            margin: 100px;
+            padding: 100px!important;
+          }
+        `}</style>
       </section>
     );
   }
